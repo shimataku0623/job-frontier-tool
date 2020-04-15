a65158e3149f80a26798c42edd5317043e6f8364
shimataku0623 <yumesya9@yahoo.co.jp>
2020-04-15 17:59:33 +0900

    取得core部分完成

--- main/core.py
+++ main/core.py
@@ -1,46 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
-	
-#r = requests.get("https://doda.jp/DodaFront/View/JobSearchDetail/j_jid__3003824339/-fm__list/-tp__1/-mtmd__0/?from=list_item")
-r = requests.get("https://doda.jp/DodaFront/View/JobSearchDetail/j_jid__3003864701/-fm__list/-tp__1/-mtmd__0/?from=list_item")
-	
-soup = BeautifulSoup(r.content, "html.parser")
-#souphead = BeautifulSoup(r, "html.parser")
-
-#企業名取得
-company = soup.find(class_ = "job_title")
-
-
-#ニュース一覧のテキストのみ抽出
-# print(soup.find(id="company_profile_table"))
-table = soup.find("table",{"id":"company_profile_table"})
-rows = table.findAll("tr")
-#td = rows[1].find("td")
-
-print(company.text)
-# print(rows[1].find("td").text) #住所
-# print(rows[-1].find("td").text) #URL
-#print(rows[6].find("td").text) #売上
-#print(rows[4].find("td").text) #社員数
-#企業によって数が違う8がないことも
-
-# print(rows[1].text)
-# print("所在地" in rows[1].text)
 
 # 出力変数の初期化
-address,url,amount,employees = "","","",""
+company,address,url,amount,employees = "","","","",""
 
-#合致するか確かめながらそれぞれ値を代入
+# 該当企業の読み込み（別ファイルで取得した方がいいかも）
+r = requests.get("https://doda.jp/DodaFront/View/JobSearchDetail/j_jid__3003824339/-fm__list/-tp__1/-mtmd__0/?from=list_item")
+
+# ページ内容の取得と解析
+soup = BeautifulSoup(r.content, "html.parser")
+
+# 企業情報該当テーブルの特定
+table = soup.find("table",{"id":"company_profile_table"})
+rows = table.findAll("tr")
+
+# 欲しい情報を文字列検索しながら変数に代入
 for item in rows:
     if "所在地" in item.text:
         address = item.find("td").text
     if "企業URL" in item.text:
-        url = item.find("td").text
+        url = item.find("td").text.strip()
     if "売上高" in item.text:
         amount = item.find("td").text
     if "従業員数" in item.text:
         employees = item.find("td").text
 
+# 企業名取得
+company = soup.find(class_ = "job_title").text
+
+# 出力
+print("企業名:",company)
 print("住所:",address)
 print("URL:",url)
 print("売り上げ:",amount)
