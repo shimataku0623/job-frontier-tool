5931acced6f1451c4dc58d730e34896f00252659
shimataku0623 <yumesya9@yahoo.co.jp>
2020-06-04 11:10:30 +0900

    sheetname change

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # job-frontier-tool
 スクレイピングツール
 
-
 # 仮想環境py3envを有効化する
 source py3env/bin/activate
  
--- test.py
+++ test.py
@@ -1,25 +1,2 @@
-import gspread
-import json
-	
-from oauth2client.service_account import ServiceAccountCredentials 
-
-#2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-
-#認証情報設定
-#ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
-credentials = ServiceAccountCredentials.from_json_keyfile_name('job-search-274909-9959e9be41a1.json', scope)
-
-#OAuth2の資格情報を使用してGoogle APIにログインします。
-gc = gspread.authorize(credentials)
-
-#共有設定したスプレッドシートキーを変数[SPREADSHEET_KEY]に格納する。
-SPREADSHEET_KEY = '1ERDY6F1mhIz1okJvXW8JHF92tq8TiSUqyvouGvXlvII'
-
-#共有設定したスプレッドシートのシート1を開く
-workbook = gc.open_by_key(SPREADSHEET_KEY)
-worksheet = workbook.worksheet('test')
-
-#更新
-header_list = ["会社名test","住所","URL","売上高","社員数"]
-worksheet.append_row(header_list)
+file_path = input("文字：")
+print("Hello " + file_path)
